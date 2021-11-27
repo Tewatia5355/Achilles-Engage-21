e1e433c069f4a8f7e275dffec8fbd218ddda61ba
Ubuntu <azureuser@Achilles-engage.2yad4dmetryevbvrk00nzajfqd.ix.internal.cloudapp.net>
2021-11-27 06:39:47 +0000

    14: Hosted

--- eng/urls.py
+++ eng/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
@@ -21,3 +22,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("auth.urls")),
 ]
+urlpatterns += staticfiles_urlpatterns()
